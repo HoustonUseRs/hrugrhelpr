979b95fef5ed8d6e99e231cc79c6e8adaeebd91c
Ed Goodwin <ed.registration@gmail.com>
2016-06-26 00:01:49 -0500

    * added header comment block to file

--- R/replace_na_with_zeros.R
+++ R/replace_na_with_zeros.R
@@ -1,3 +1,17 @@
+##
+## [replace_na_with_zeros.R]
+##
+## author     : Dinesh Majeti
+## project    : hrugrhelpr
+## createdate : 06.26.2016
+##
+## description:
+##    replace NA items with zeros in a vector
+##
+## version: 0.01
+## changelog:
+##
+
 replaceNAwithZero <- function(y) {
   ## replace NAs with R in a vector
   y[is.na(y)] <- 0
