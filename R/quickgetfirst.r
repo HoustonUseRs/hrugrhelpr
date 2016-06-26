87cd153355cd802ed797c3bbf88218a72e3e1868
Ed Goodwin <ed.registration@gmail.com>
2016-06-26 00:03:39 -0500

    * added header to file with project details

--- R/quickgetfirst.r
+++ R/quickgetfirst.r
@@ -1,3 +1,18 @@
+##
+## [quickgetfirst.r]
+##
+## author     : Amanda Shih
+## project    : hrugrhelpr
+## createdate : 06.25.2016
+##
+## description:
+##    return the first item in a vector
+##
+## version: 0.01
+## changelog:
+##
+
+
 get_first <- function(vec) {
   vec[1]
 }
