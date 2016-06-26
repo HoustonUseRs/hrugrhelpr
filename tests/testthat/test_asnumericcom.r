21ae4bca40433b882b304e376d1142c941e1fbae
Ed Goodwin <ed.registration@gmail.com>
2016-06-26 16:18:08 -0500

    * added header data to test_asnumericcom.r test file

--- tests/testthat/test_asnumericcom.r
+++ tests/testthat/test_asnumericcom.r
@@ -1,3 +1,17 @@
+##
+## [test_asnumericcom.r]
+##
+## author     : Ed Goodwin
+## project    : hrugrhelpr
+## createdate : 06.26.2016
+##
+## description:
+##    test for as.numeric.com() function in hrugrhelpr
+##
+## version: 0.01
+## changelog:
+##
+
 context("as.numeric.com tests")
 test_that("Test comma-string to numeric", {
   x = "1,010"
